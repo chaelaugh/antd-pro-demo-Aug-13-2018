aa16d52cf37f40ef3983fa31d4042d87be1db41d
人生海海 <quinlandg@outlook.com>
2018-08-22 10:09:57 +0800

    push

--- src/routes/BidManagement/BidList.js
+++ src/routes/BidManagement/BidList.js
@@ -1,16 +1,50 @@
 import React, { Component, Fragment } from 'react';
 import { connect } from 'dva';
-import StepsCom from './StepsCom';
+import { Modal, Input, Button } from 'antd';
+
 @connect()
 export default class BidList extends Component {
-  state = {};
+  state = {
+    visible: false,
+    value: '',
+  };
+
+  onChange = (e) => {
+    const { value } = e.target;
+    this.setState({
+      value,
+    })
+  }
+
+  onShow = () => {
+    this.setState({
+      visible: true,
+    })
+  }
+
+  onCancel = () => {
+    this.setState({
+      visible: false,
+    })
+  }
 
   render() {
+    const { visible, value } = this.state;
     return (
       <Fragment>
+        <Button onClick={this.onShow}>Click</Button>
         <div style={{ padding: 20 }}>
-          <StepsCom />
+          <Input value={value} />
         </div>
+        <Modal
+          title="Basic Modal"
+          visible={visible}
+          onOk={this.handleOk}
+          onCancel={this.onCancel}
+          footer={null}
+        >
+          <Input onBlur={this.onChange} />
+        </Modal>
       </Fragment>
     );
   }
